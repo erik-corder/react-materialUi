b45a848da6757c55697b5de17262ba1d76e9f613
kanishkaisuru <sampathisuru516@gmail.com>
2020-12-26 15:51:36 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, makeStyles } from "@material-ui/core";
+import "./App.css";
+import Header from "./components/Header";
+import SideMenu from "./components/SideMenu";
+
+const useStyle = makeStyles({
+  appMain:{
+    paddingLeft: '320px',
+    width: '100%'
+  }
+});
 
 function App() {
+
+  const classes = useStyle();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SideMenu />
+      <div className={classes.appMain}>
+        <Header />
+      </div>
+      <CssBaseline />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,78 @@
+import {
+  AppBar,
+  Badge,
+  Grid,
+  IconButton,
+  InputBase,
+  makeStyles,
+  Toolbar,
+} from "@material-ui/core";
+import React from "react";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import SearchIcon from "@material-ui/icons/Search";
+import { green } from "@material-ui/core/colors";
+
+const useStyle = makeStyles(theme => ({
+  root: {
+    backgroundColor: "#fff",
+  },
+  searchInput: {
+    opacity: "0.6",
+    padding: "0px 8px",
+    fontSize: "0.8rem",
+    "&:hover": {
+      backgroundColor: "#f2f2f2",
+    },
+    "& .MuiSvgIcon-root": {
+      marginRight: theme.spacing(1),
+    },
+  },
+  btnRoot: {
+    backgroundColor: "green",
+  },
+  btnLable: {
+    backgroundColor: "red",
+  },
+}));
+
+const Header = () => {
+  const classes = useStyle();
+
+  return (
+    <AppBar position="static" className={classes.root}>
+      <Toolbar>
+        <Grid container alignItems="center">
+          <Grid item>
+            <InputBase
+              placeholder="Search topics"
+              className={classes.searchInput}
+              startAdornment={<SearchIcon fontSize="small" />}
+            />
+          </Grid>
+          <Grid item sm></Grid>
+          <Grid item>
+            <IconButton classes={{ root: classes.btnRoot, label: classes.btnLable }}>
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsNoneIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge badgeContent={4} color="primary">
+                <ChatBubbleOutlineIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge>
+                <PowerSettingsNewIcon />
+              </Badge>
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
